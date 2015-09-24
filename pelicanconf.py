cb8c2a6ba8715eb8caa26910101b482aa5f7941e
Himanshu Mishra <himanshu2014iit@gmail.com>
2015-09-24 18:53:02 +0530

    Fix the site-url for correct css references

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u"MA '19"
 SITENAME = u'The Mlog'
-SITEURL = ''
+SITEURL = 'http://kgpmnc.github.io/blog'
 
 PATH = 'content'
 
@@ -22,12 +22,9 @@ AUTHOR_FEED_RSS = None
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Facebook Group', 'https://www.facebook.com/groups/mnc14'),
 
 DEFAULT_PAGINATION = 10
 
